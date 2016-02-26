a4e4a96dfd9b4ed3458ff44fbcd388d8232020e4
Chase Bolt <cbolt@denirostaff.com>
2016-02-25 22:09:06 -0800

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name 'polycom-boot-server'
+maintainer 'Chase Bolt'
+maintainer_email 'chase.bolt@gmail.com'
+license 'Apache 2.0'
+description 'Installs and configures a boot server to configure polycom phones'
+version '0.1.0'
+
+source_url 'https://github.com/chasebolt/polycom-boot-server'
+issues_url 'https://github.com/chasebolt/polycom-boot-server/issues'
+
+supports 'amazon'
+supports 'centos'
+supports 'fedora'
+supports 'oracle'
+supports 'redhat'
+supports 'scientific'
